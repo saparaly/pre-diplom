7072902a2074023733dfd10374d7e012ba411a44
ssengerb <sanzhar_1710@mail.ru>
2023-04-17 23:19:11 +0600

    modified show snippet method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 )
 
 // userIdPost is loged in users id
@@ -48,4 +50,57 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		UserID: userid,
 	})
 	// fmt.Println("666")
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		ErrorHandler(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		app.clienrError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	post, err := app.posts.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			// app.notFound(w)
+			ErrorHandler(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		} else {
+			ErrorHandler(w, http.StatusText(http.StatusInternalServerError), 500)
+			// app.serverError(w, err)
+		}
+		return
+	}
+
+	if post == nil {
+		// app.notFound(w)
+		ErrorHandler(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	comments, err := app.posts.GetComments(post.Id)
+	if err != nil {
+		// app.serverError(w, err)
+		ErrorHandler(w, http.StatusText(http.StatusInternalServerError), 500)
+		return
+	}
+
+	files := []string{
+		"./ui/html/show.page.html",
+		"./ui/html/base.layout.html",
+		"./ui/html/footer.html",
+	}
+
+	userid := app.GetUserIDForUse(w, r)
+	// Use the new render helper.
+	app.render(w, r, files, &templateData{
+		Post:     post,
+		Comments: comments,
+		UserID:   userid,
+	})
 }
